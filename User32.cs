237eccfad85ceee7bbfa1648840abb97f7def15a
randall.shattuck@gmail.com <randall.shattuck@gmail.com@c0d1fedd-9a91-f800-9ccc-a2bced6221b8>
2011-05-04 01:43:52 +0000

    Added EnumWindows definition

--- User32.cs
+++ User32.cs
@@ -5,5 +5,15 @@ namespace API.Win32
 {
     static class User32
     {
+        [DllImport(
+            "User32.dll",
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.Winapi
+            )]
+        public static extern
+        bool EnumWindows(
+            [In] EnumWindowsProc lpEnumFunc,
+            [In] IntPtr lParam
+            );
     }
 }
